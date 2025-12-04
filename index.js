5b45b8d9ab03ca0906f0846bf78973e307e1e949
it me <fake@gmail.com>
2025-12-04 13:51:26 +0300

    12

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
+//  Задача 1
+function concatenateStrings(a, b) {
 
+    let concatenated = a + b;
+    let result = concatenated.replace(/\s+/g, '');
+    return result;
+}
+
+//  Задача 2
+function getStringLength(str) {
+    return str.length;
+}
+
+//  Задача 3
+function getStringFromTemplate(firstName, lastName) {
+    return `Hello, ${firstName} ${lastName}!`;
+}
+
+//  Задача 4
+function getChar(str, index) {
+    return str[index - 1];
+}
+
+//  Задача 5
+function removeFirstOccurrences(str, value) {
+    const index = str.indexOf(value);
+    if (index === -1) {
+        return str;
+    }
+    return str.slice(0, index) + str.slice(index + value.length);
+}
+
+//  Задача 6
+const getRectangleString = (w, h) => {
+    let result = '';
+    for (let i = 0; i < h; i++) {
+        result += '*'.repeat(w) + '\n';
+    }
+    return result.trim();
+}
